3468709b22850c9b0cfae78286f19c369957e84d
Julia Samól <julia.samol@gmail.com>
2019-04-19 00:32:44 +0200

    Create Keycloak security configuration

---
+++ src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/configuration/SecurityConfiguration.kt
@@ -0,0 +1,49 @@
+package pl.edu.agh.bioauth.appregistrationbackend.configuration
+
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = [KeycloakSecurityComponents::class])
+class SecurityConfiguration : KeycloakWebSecurityConfigurerAdapter() {
+
+    @get:Bean("keycloakConfigResolver")
+    val keycloakConfigResolver: KeycloakConfigResolver = KeycloakSpringBootConfigResolver()
+
+    @Bean
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
+            RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+
+    override fun configure(http: HttpSecurity?) {
+        super.configure(http)
+        http?.authorizeRequests()
+                ?.antMatchers("/api/**/*")
+                ?.hasRole("developer")
+                ?.anyRequest()
+                ?.permitAll()
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        val keycloakAuthenticationProvider = KeycloakAuthenticationProvider().apply {
+            setGrantedAuthoritiesMapper(SimpleAuthorityMapper())
+        }
+        auth.authenticationProvider(keycloakAuthenticationProvider)
+    }
+}
