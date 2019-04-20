35ee431b030d8616f64654cb08b546814a9ae552
Julia Samól <julia.samol@gmail.com>
2019-04-20 03:30:03 +0200

    Setup CORS

--- src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/configuration/SecurityConfiguration.kt
@@ -5,11 +5,11 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -17,15 +17,32 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
-import pl.edu.agh.bioauth.appregistrationbackend.util.UserRole
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import pl.edu.agh.bioauth.appregistrationbackend.util.allowedHttpMethods
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @ComponentScan(basePackageClasses = [KeycloakSecurityComponents::class])
 class SecurityConfiguration : KeycloakWebSecurityConfigurerAdapter() {
 
     @get:Bean("keycloakConfigResolver")
     val keycloakConfigResolver: KeycloakConfigResolver = KeycloakSpringBootConfigResolver()
+
+    @get:Bean("corsConfigurationSource")
+    val corsConfigurationSource: CorsConfigurationSource
+        get() {
+            val configuration = CorsConfiguration().apply {
+                allowedOrigins = listOf("http://localhost:3000")
+                allowedHttpMethods = listOf(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.OPTIONS)
+                allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
+            }
+
+            return UrlBasedCorsConfigurationSource().apply {
+                registerCorsConfiguration("/**", configuration)
+            }
+        }
 
     @Bean
     override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy =
@@ -33,9 +50,12 @@ class SecurityConfiguration : KeycloakWebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         super.configure(http)
-        http?.authorizeRequests()
-                ?.antMatchers("/api/**/*")
-                ?.hasRole(UserRole.DEVELOPER)
+
+        http?.cors()
+                ?.and()
+                ?.authorizeRequests()
+                ?.antMatchers("/v1/**")
+                ?.authenticated()
                 ?.anyRequest()
                 ?.permitAll()
     }
--- src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/util/UserRole.kt
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.bioauth.appregistrationbackend.util
-
-object UserRole {
-    const val DEVELOPER = "developer"
-}
