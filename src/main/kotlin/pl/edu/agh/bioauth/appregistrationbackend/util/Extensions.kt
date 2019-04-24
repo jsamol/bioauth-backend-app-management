749e05a40014a98e624ac8cfa344ec649bb1fb58
Julia Samól <julia.samol@gmail.com>
2019-04-24 23:05:15 +0200

    Remove unused extensions

--- src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/util/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.bioauth.appregistrationbackend.util
-
-import org.springframework.http.HttpMethod
-import org.springframework.web.cors.CorsConfiguration
-
-var CorsConfiguration.allowedHttpMethods: List<HttpMethod>?
-    get() = this.allowedMethods?.mapNotNull { HttpMethod.resolve(it) }
-    set(value) { this.allowedMethods = value?.map { it.name } }
