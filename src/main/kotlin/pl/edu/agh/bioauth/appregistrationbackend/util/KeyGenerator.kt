100f07f1cf7df69e1598d64684a7b9df9c530de1
Julia Samól <julia.samol@gmail.com>
2019-04-25 00:23:04 +0200

    Create object for AppId and AppSecret generation

---
+++ src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/util/KeyGenerator.kt
@@ -0,0 +1,12 @@
+package pl.edu.agh.bioauth.appregistrationbackend.util
+
+object KeyGenerator {
+
+    fun getAppId(): String {
+        return "123"
+    }
+
+    fun getAppSecret(): String {
+        return "123"
+    }
+}
