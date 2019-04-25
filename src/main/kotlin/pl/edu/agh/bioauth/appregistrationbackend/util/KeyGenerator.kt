74fee037bd97c920c40b4794a24bcdf8d7eaa299
Julia Samól <julia.samol@gmail.com>
2019-04-25 16:45:07 +0200

    Implement app secret generation

--- src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/util/KeyGenerator.kt
+++ src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/util/KeyGenerator.kt
@@ -2,20 +2,33 @@ package pl.edu.agh.bioauth.appregistrationbackend.util
 
 import com.google.common.hash.Hashing
 import java.nio.charset.StandardCharsets
+import java.security.SecureRandom
 
 object KeyGenerator {
 
+    private const val APP_ID_LENGTH = 32
+    private const val APP_SECRET_SIZE = 32
+
+    private val secureRandom: SecureRandom
+        get() = SecureRandom()
+
     fun getAppId(userId: String, name: String): String {
         val rawId = "$userId-$name-${System.currentTimeMillis()}"
-        val appId = Hashing
+
+        return Hashing
                 .sha256()
                 .hashString(rawId, StandardCharsets.UTF_8)
                 .toString()
-
-        return appId.substring(0, appId.length / 2)
+                .substring(0, APP_ID_LENGTH)
     }
 
     fun getAppSecret(): String {
-        return "123"
+        val rawSecretBytes = ByteArray(APP_SECRET_SIZE).also { secureRandom.nextBytes(it) }
+        val rawSecret = rawSecretBytes.joinToString("") { String.format("%02X", it) }
+
+        return Hashing
+                .sha256()
+                .hashString(rawSecret, StandardCharsets.UTF_8)
+                .toString()
     }
 }
