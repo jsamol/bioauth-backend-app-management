65c7eed1e15158406e96680d113777237c8dcdf4
Julia Samól <julia.samol@gmail.com>
2019-04-26 20:44:13 +0200

    Create app profiles

--- src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/AppManagementApplication.kt
+++ src/main/kotlin/pl/edu/agh/bioauth/appregistrationbackend/AppManagementApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppRegistrationBackendApplication
+class AppManagementApplication
 
 fun main(args: Array<String>) {
-    runApplication<AppRegistrationBackendApplication>(*args)
+    runApplication<AppManagementApplication>(*args)
 }
